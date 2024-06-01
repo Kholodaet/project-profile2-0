79169fb8eb90a03c2f75e67a514ed58cf7d06bbf
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-06-01 22:39:14 +0300

    css converted

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,10 +1,60 @@
+import { fetchDataAndInitializeSwiper } from '../js/reviews/reviews-api.js';
+import { createMarkupReview } from '../js/reviews/render-functions.js';
 
-import { fetchDataAndInitializeSwiper } from './reviews/reviews-api';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const list = document.querySelector('#review-swiper-list');
+
+export const initializeSwiper = async () => {
+  try {
+    const data = await fetchDataAndInitializeSwiper();
+    const markup = createMarkupReview(data);
+
+    if (!markup) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Sorry, reviews not found.',
+        position: 'bottomLeft',
+      });
+      return;
+    }
+    list.insertAdjacentHTML('beforeend', markup);
+
+    const swiper3 = new Swiper('#swiper3', {
+      direction: 'horizontal',
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 16,
+        },
+        1280: {
+          slidesPerView: 2,
+          spaceBetween: 18,
+        },
+      },
+      navigation: {
+        nextEl: '.btn-next-review',
+        prevEl: '.btn-prev-review',
+      },
+    });
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Error while fetching reviews from server',
+      position: 'bottomLeft',
+    });
+  }
+};
+
 
 export function initReviewsSection() {
-  async function initializeSwiper() {
-     await fetchDataAndInitializeSwiper();
-  }
-
   initializeSwiper();
 }
