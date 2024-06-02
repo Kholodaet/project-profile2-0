30ccf7b0dbad466a15a088baca4d04ee3f6b0313
Kinderpro <131799650+Kinderpro@users.noreply.github.com>
2024-06-02 16:38:30 +0300

    header, mobile-menu

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ initReviewsSection();
 //=======
 
 //<<<<<<< HEAD
+
+//>>>>>>> header
+import { toggleMenu } from './js/header.js';
+toggleMenu();
+//>>>>>>> header
+
 //>>>>>>> main
 
 //<<<<<<< My-project
